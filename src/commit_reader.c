26b260b050d2a8ba486c0beaea687cc29ca9f1f6
kenhys <hayashi@clear-code.com>
2013-03-10 01:08:39 +0900

    implement html preview

---
+++ src/commit_reader.c
@@ -0,0 +1,517 @@
+/*
+ * Commit Reader -- 
+ * Copyright (C) 2013 HAYASHI Kentaro
+ *
+ */
+
+#include "config.h"
+#include <glib.h>
+#include <glib/gi18n-lib.h>
+#include <glib/gprintf.h>
+#include <gtk/gtk.h>
+#include <libintl.h>
+
+#if defined(USE_WEBKITGTK)
+#include <webkit/webkit.h>
+#include <webkit/webkitwebview.h>
+#elif defined(USE_GTKHTML)
+#include <gtkhtml/gtkhtml.h>
+#else
+#error "Not Implemented any HTML features"
+#endif
+
+#include "sylmain.h"
+#include "plugin.h"
+#include "folder.h"
+#include "procmsg.h"
+#include "compose.h"
+#include "alertpanel.h"
+#include "foldersel.h"
+#include "headerview.h"
+#include "messageview.h"
+#include "procheader.h"
+
+#include "copying.h"
+#include "commit_reader.h"
+
+static SylPluginInfo info = {
+  N_(PLUGIN_NAME),
+  "0.1.0",
+  "HAYASHI Kentaro",
+  N_(PLUGIN_DESC)
+};
+
+CommitReaderOption SYLPF_OPTION;
+
+static void init_done_cb(GObject *obj, gpointer data);
+static void app_exit_cb(GObject *obj, gpointer data);
+
+gulong app_exit_handler_id = 0;
+
+void plugin_load(void)
+{
+  gpointer mainwin;
+
+  syl_init_gettext(COMMIT_READER, "lib/locale");
+  g_print("sylicons plug-in loaded!\n");
+
+  syl_plugin_add_menuitem("/Tools", NULL, NULL, NULL);
+  syl_plugin_add_menuitem("/Tools",
+                          _("CommitReader [commit_reader]"),
+                          exec_commit_reader_menu_cb, NULL);
+
+  mainwin = syl_plugin_main_window_get();
+  syl_plugin_main_window_popup(mainwin);
+
+  g_signal_connect_after(syl_app_get(), "init-done", G_CALLBACK(init_done_cb),
+                         NULL);
+  g_signal_connect(syl_app_get(), "app-exit", G_CALLBACK(app_exit_cb), NULL);
+
+  syl_plugin_signal_connect("messageview-show",
+                            G_CALLBACK(messageview_show_cb), NULL);
+
+  sylpf_load_option_rcfile((SylPluginFactoryOption*)&SYLPF_OPTION,
+                           COMMIT_READER_RC);
+}
+
+void plugin_unload(void)
+{
+  SYLPF_START_FUNC;
+  SYLPF_END_FUNC;
+}
+
+SylPluginInfo *plugin_info(void)
+{
+  return &info;
+}
+
+gint plugin_interface_version(void)
+{
+    return 0x0108;
+    /* return SYL_PLUGIN_INTERFACE_VERSION; */
+}
+
+static void init_done_cb(GObject *obj, gpointer data)
+{
+  SYLPF_START_FUNC;
+  SYLPF_END_FUNC;
+}
+
+static void app_exit_cb(GObject *obj, gpointer data)
+{
+  SYLPF_START_FUNC;
+  SYLPF_END_FUNC;
+}
+
+static void exec_commit_reader_menu_cb(void)
+{
+  GtkWidget *dialog;
+  gint response;
+  
+  SYLPF_START_FUNC;
+
+  dialog = create_preference_dialog(&SYLPF_OPTION);
+  
+  gtk_widget_show_all(dialog);
+  response = gtk_dialog_run(GTK_DIALOG(dialog));
+
+  switch (response) {
+  case GTK_RESPONSE_OK:
+    save_htmlview_preference(&SYLPF_OPTION);
+    break;
+  case GTK_RESPONSE_CANCEL:
+  default:
+    break;
+  }
+
+  gtk_widget_destroy(dialog);
+  
+  SYLPF_END_FUNC;
+}
+
+static GtkWidget *create_preference_dialog(CommitReaderOption *option)
+{
+  GtkWidget *vbox, *hbox;
+  GtkWidget *dialog;
+  gint width, height;
+  gpointer mainwin;
+  GtkWidget *window;
+  
+  SYLPF_START_FUNC;
+
+
+  dialog = gtk_dialog_new_with_buttons(_("CommitReader"),
+                                       GTK_WINDOW(window),
+                                       GTK_DIALOG_MODAL,
+                                       GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                       GTK_STOCK_OK, GTK_RESPONSE_OK,
+                                       NULL);
+
+  sylpf_init_preference_dialog_size(dialog);
+
+  vbox = gtk_vbox_new(FALSE, SYLPF_BOX_SPACE);
+  hbox = gtk_hbox_new(TRUE, SYLPF_BOX_SPACE);
+
+  gtk_container_add(GTK_CONTAINER(hbox), vbox);
+  gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), hbox);
+
+  GtkWidget *notebook = gtk_notebook_new();
+  create_config_main_page(notebook, SYLPF_OPTION.rcfile);
+  create_config_about_page(notebook, SYLPF_OPTION.rcfile);
+
+  gtk_widget_show(notebook);
+  
+  gtk_box_pack_start(GTK_BOX(vbox), notebook, TRUE, TRUE, 0);
+  
+  gtk_box_set_spacing(GTK_BOX(GTK_DIALOG(dialog)->vbox), SYLPF_BOX_SPACE);
+
+  SYLPF_RETURN_VALUE(dialog);
+  
+}
+
+static GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey)
+{
+  GtkWidget *vbox;
+  GtkWidget *private, *image, *scripts, *switch_tab;
+  
+  SYLPF_START_FUNC;
+
+  if (notebook == NULL){
+    return NULL;
+  }
+  /* startup */
+  if (pkey!=NULL){
+  }
+  vbox = gtk_vbox_new(FALSE, 6);
+
+#if USE_WEBKITGTK
+  SYLPF_OPTION.private_browsing = gtk_check_button_new_with_label(_("Enable private browsing."));
+  private = sylpf_pack_widget_with_aligned_frame(SYLPF_OPTION.private_browsing, _("Privacy"));
+
+  SYLPF_OPTION.load_image = gtk_check_button_new_with_label(_("Enable auto load image."));
+  image = sylpf_pack_widget_with_aligned_frame(SYLPF_OPTION.load_image, _("Image"));
+
+  SYLPF_OPTION.scripts = gtk_check_button_new_with_label(_("Enable scripts."));
+  scripts = sylpf_pack_widget_with_aligned_frame(SYLPF_OPTION.scripts, _("Scripting"));
+
+#endif
+
+  SYLPF_OPTION.switch_tab = gtk_check_button_new_with_label(_("Show HTML tab as default."));
+  switch_tab = sylpf_pack_widget_with_aligned_frame(SYLPF_OPTION.switch_tab, _("HTML Tab"));
+
+  gtk_box_pack_start(GTK_BOX(vbox), switch_tab, FALSE, FALSE, 0);
+
+#if USE_WEBKITGTK
+  gtk_box_pack_start(GTK_BOX(vbox), private, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), image, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), scripts, FALSE, FALSE, 0);
+#endif
+
+  sylpf_load_option_rcfile((SylPluginFactoryOption*)&SYLPF_OPTION,
+                           COMMIT_READER_RC);
+
+#if USE_WEBKITGTK
+  SYLPF_OPTION.private_flag = SYLPF_GET_RC_BOOLEAN("enable-private-browsing");
+  SYLPF_OPTION.image_flag = SYLPF_GET_RC_BOOLEAN("auto-load-images");
+  SYLPF_OPTION.script_flag = SYLPF_GET_RC_BOOLEAN("enable-scripts");
+#endif
+
+  SYLPF_OPTION.switch_tab_flag = SYLPF_GET_RC_BOOLEAN(ENABLE_SWITCH_TAB);
+
+#define TOGGLE_STATE(widget, state) \
+  gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(SYLPF_OPTION.widget), SYLPF_OPTION.state)
+
+#if USE_WEBKITGTK
+  TOGGLE_STATE(private_browsing, private_flag);
+  TOGGLE_STATE(load_image, image_flag);
+  TOGGLE_STATE(scripts, script_flag);
+#endif
+  TOGGLE_STATE(switch_tab, switch_tab_flag);
+
+  save_option_rcfile();
+
+  GtkWidget *general_lbl = gtk_label_new(_("General"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), vbox, general_lbl);
+  gtk_widget_show_all(notebook);
+
+  SYLPF_RETURN_VALUE(vbox);
+}
+
+static GtkWidget *create_config_about_page(GtkWidget *notebook, GKeyFile *pkey)
+{
+  GtkWidget *hbox, *vbox;
+  GtkWidget *label;
+  GtkWidget *scrolled, *tview;
+  GtkTextBuffer *tbuffer;
+
+  SYLPF_START_FUNC;
+
+  g_return_val_if_fail(notebook != NULL, NULL);
+
+  hbox = gtk_hbox_new(TRUE, SYLPF_BOX_SPACE);
+  vbox = gtk_vbox_new(FALSE, SYLPF_BOX_SPACE);
+  gtk_box_pack_start(GTK_BOX(hbox), vbox, TRUE, TRUE, SYLPF_BOX_SPACE);
+
+  label = gtk_label_new(COMMIT_READER);
+  gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, TRUE, SYLPF_BOX_SPACE);
+
+  label = gtk_label_new(PLUGIN_DESC);
+  gtk_box_pack_start(GTK_BOX(vbox), label, FALSE, TRUE, SYLPF_BOX_SPACE);
+
+  scrolled = gtk_scrolled_window_new(NULL, NULL);
+
+  tbuffer = gtk_text_buffer_new(NULL);
+  gtk_text_buffer_set_text(tbuffer, _(copyright), strlen(copyright));
+  tview = gtk_text_view_new_with_buffer(tbuffer);
+  gtk_text_view_set_editable(GTK_TEXT_VIEW(tview), FALSE);
+  gtk_container_add(GTK_CONTAINER(scrolled), tview);
+
+  gtk_box_pack_start(GTK_BOX(vbox), scrolled, TRUE, TRUE, SYLPF_BOX_SPACE);
+
+  label = gtk_label_new(_("About"));
+  gtk_notebook_append_page(GTK_NOTEBOOK(notebook), hbox, label);
+  gtk_widget_show_all(notebook);
+
+  SYLPF_RETURN_VALUE(NULL);
+}
+
+static void messageview_show_cb(GObject *obj, gpointer msgview,
+				MsgInfo *msginfo, gboolean all_headers)
+{
+  MessageView *messageview;
+  HeaderView *headerview;
+  GtkWidget *hbox;
+  GList *wl;
+  gchar *msg_path;
+  char **to_list;
+  gsize n_to_items;
+  GError error;
+  FILE *msg_file, *input;
+  gchar *html_buf, *text_buf;
+  gint i;
+  MimeInfo *mimeinfo, *partial;
+#if defined(USE_WEBKITGTK)
+  WebKitWebSettings *settings = NULL;
+#elif defined(USE_GTKHTML)
+#else
+#error "Not Implemented"
+#endif
+  
+  SYLPF_START_FUNC;
+
+#if DEBUG
+  g_print("[DEBUG] sylicons: %p: messageview_show (%p), all_headers: %d: %s\n",
+	  obj, msgview, all_headers,
+	  msginfo && msginfo->subject ? msginfo->subject : "");
+#endif
+  
+  if (!msgview) {
+    SYLPF_DEBUG_STR("msgview is NULL");
+    return;
+  }
+
+  messageview = (MessageView*)msgview;
+  if (!messageview) {
+    SYLPF_DEBUG_STR("messageview is NULL");
+    return;
+  }
+
+  headerview = messageview->headerview;
+  if (!headerview) {
+    SYLPF_DEBUG_STR("headerview is NULL");
+    return;
+  }
+
+  hbox = headerview->hbox;
+  if (!hbox) {
+    SYLPF_DEBUG_STR("headerview hbox is NULL");
+    return;
+  }
+
+  wl = gtk_container_get_children(GTK_CONTAINER(hbox));
+
+  msg_path = procmsg_get_message_file_path(msginfo);
+
+  SYLPF_DEBUG_STR("msg_path", msg_path);
+
+  GList* hl = procheader_get_header_list_from_file(msg_path);
+  gchar *path = NULL;
+
+  to_list = SYLPF_GET_RC_STRING_LIST(SYLPF_OPTION.rcfile,
+                                     COMMIT_READER,
+                                     "commit-to",
+                                     &n_to_items,
+                                     &error);
+  g_return_if_fail(n_to_items > 0);
+
+  if (!to_list) {
+    SYLPF_WARN_MSG("key commit-to not found in rc file");
+    return;
+  }
+
+  for (i = 0; i < n_to_items; i++) {
+    if (msginfo && msginfo->to && to_list[i]) {
+      SYLPF_DEBUG_STR("msginfo->to", msginfo->to);
+      SYLPF_DEBUG_STR("to_list[i]", to_list[i]);
+      if (strcmp(msginfo->to, to_list[i]) == 0) {
+        SYLPF_DEBUG_STR("matched to 'commit-to'", msginfo->to);
+
+        if (SYLPF_OPTION.html_view == NULL) {
+          SYLPF_OPTION.html_view = create_htmlview(GTK_NOTEBOOK(messageview->notebook));
+        }
+        text_buf = sylpf_get_text_from_message_partial(msginfo, MIME_TEXT);
+        
+        SYLPF_DEBUG_STR("text/plain", text_buf);
+
+        html_buf = sylpf_format_diff2html_text(text_buf);
+          
+#if defined(USE_WEBKITGTK)
+        settings = webkit_web_view_get_settings(SYLPF_OPTION.html_view);
+
+        g_object_set(G_OBJECT(settings), ENABLE_IMAGES, SYLPF_OPTION.image_flag, NULL);
+        g_object_set(G_OBJECT(settings), ENABLE_SCRIPTS, SYLPF_OPTION.script_flag, NULL);
+        g_object_set(G_OBJECT(settings), ENABLE_PRIVATE_BROWSING, SYLPF_OPTION.private_flag, NULL);
+
+        g_object_set(G_OBJECT(settings), DEFAULT_FONT_SIZE, SYLPF_OPTION.font_size, NULL);
+
+        webkit_web_view_set_settings(SYLPF_OPTION.html_view, settings);
+
+        webkit_web_view_load_string(SYLPF_OPTION.html_view, html_buf, NULL, NULL, "");
+
+#elif defined(USE_GTKHTML)
+        gtk_html_load_from_string(GTK_HTML(SYLPF_OPTION.html_view), html_buf, -1);
+#else
+#error "use WebKitGTK or GtkHTML"
+#endif
+
+        g_free(text_buf);
+
+        mimeinfo = procmime_scan_message(msginfo);
+
+        messageview_change_view_type(messageview, MVIEW_MIME);
+        gtk_notebook_set_current_page(GTK_NOTEBOOK(messageview->notebook), 2);
+        
+        if (SYLPF_OPTION.switch_tab_flag != FALSE) {
+        }
+      }
+
+    }
+  }
+  g_strfreev(to_list);
+
+  SYLPF_END_FUNC;
+}
+
+static void save_commit_reader_preference(CommitReaderOption *option)
+{
+  SYLPF_START_FUNC;
+
+#define TOGGLE_STATE(widget) \
+  gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(widget))
+
+#if USE_WEBKITGTK
+  option->private_flag = TOGGLE_STATE(option->private_browsing);
+  option->image_flag = TOGGLE_STATE(option->load_image);
+  option->script_flag = TOGGLE_STATE(option->scripts);
+#endif
+  option->switch_tab_flag = TOGGLE_STATE(option->switch_tab);
+
+#if USE_WEBKITGTK
+  g_print("%s:%s\n", ENABLE_PRIVATE_BROWSING, BOOL_TOSTRING(option->private_flag));
+  g_print("%s:%s\n", ENABLE_IMAGES, BOOL_TOSTRING(option->image_flag));
+  g_print("%s:%s\n", ENABLE_SCRIPTS, BOOL_TOSTRING(option->script_flag));
+#endif
+  g_print("%s:%s\n", ENABLE_SWITCH_TAB, BOOL_TOSTRING(option->switch_tab_flag));
+
+#undef TOGGLE_STATE
+  
+  sylpf_load_option_rcfile((SylPluginFactoryOption*)&SYLPF_OPTION,
+                           COMMIT_READER_RC);
+  
+#if USE_WEBKITGTK
+  SYLPF_SET_RC_BOOLEAN(ENABLE_PRIVATE_BROWSING, SYLPF_OPTION.private_flag);
+  SYLPF_SET_RC_BOOLEAN(ENABLE_IMAGES, SYLPF_OPTION.image_flag);
+  SYLPF_SET_RC_BOOLEAN(ENABLE_SCRIPTS, SYLPF_OPTION.script_flag);
+#endif
+  SYLPF_SET_RC_BOOLEAN(ENABLE_SWITCH_TAB, SYLPF_OPTION.switch_tab_flag);
+
+  save_option_rcfile();
+
+#undef TOGGLE_STATE
+
+  SYLPF_END_FUNC;
+}
+
+static GtkWidget *create_htmlview(GtkNotebook *notebook)
+{
+
+#if defined(USE_WEBKITGTK)
+  WebKitWebView *html_widget = WEBKIT_WEB_VIEW(webkit_web_view_new());
+#elif defined(USE_GTKHTML)
+  GtkWidget *html_widget = gtk_html_new();
+#endif
+  GtkWidget *scrolled;
+
+  SYLPF_START_FUNC;
+
+  scrolled = gtk_scrolled_window_new(NULL, NULL);
+  gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scrolled),
+                                 GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+  gtk_container_add(GTK_CONTAINER(scrolled), GTK_WIDGET(html_widget));
+  gtk_widget_show_all(GTK_WIDGET(scrolled));
+
+  gtk_container_add(GTK_CONTAINER(notebook),
+                    GTK_WIDGET(scrolled));
+  gtk_notebook_set_tab_label_text(GTK_NOTEBOOK(notebook),
+                                  GTK_WIDGET(scrolled), _("HTML"));
+
+  SYLPF_RETURN_VALUE(GTK_WIDGET(html_widget));
+}
+
+
+/* derived from messageview.c */
+static void messageview_set_mime_view_layout(MessageView *messageview)
+{
+  
+  if (SYLPF_OPTION.attach_toolbutton_pos == 0) {
+    gtk_box_set_child_packing(GTK_BOX(messageview->hbox),
+                              messageview->toolbar_vbox,
+                              FALSE, FALSE, 0, GTK_PACK_START);
+  } else {
+    gtk_box_set_child_packing(GTK_BOX(messageview->hbox),
+                              messageview->toolbar_vbox,
+                              FALSE, FALSE, 0, GTK_PACK_END);
+  }
+
+  if (SYLPF_OPTION.show_attach_tab_flag) {
+    gtk_widget_hide(messageview->toolbar_vbox);
+  } else {
+    gtk_widget_show(messageview->toolbar_vbox);
+  }
+
+  gtk_notebook_set_show_tabs(GTK_NOTEBOOK(messageview->notebook),
+                             SYLPF_OPTION.show_attach_tab_flag);
+}
+
+/* derived from messageview.c */
+static void messageview_change_view_type(MessageView *messageview,
+                                         MessageType type)
+{
+  GtkWidget *notebook = messageview->notebook;
+
+  if (type == MVIEW_MIME) {
+    messageview_set_mime_view_layout(messageview);
+    
+    gtk_notebook_set_current_page(GTK_NOTEBOOK(notebook),
+                                  messageview->current_page);
+    
+    if (messageview->current_page == 0) {
+      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(messageview->mime_toggle_btn), FALSE);
+    } else {
+      gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(messageview->mime_toggle_btn), TRUE);
+    }
+  } else {
+    return;
+  }
+  messageview->type = type;
+}
---
+++ src/commit_reader.h
@@ -0,0 +1,92 @@
+/*
+ * CommitReader -- 
+ * Copyright (C) 2013 HAYASHI Kentaro
+ *
+ */
+
+#ifndef __COMMIT_READER_H__
+#define __COMMIT_READER_H__
+
+#ifdef USE_WEBKITHTML
+#include <webkit/webkitwebview.h>
+#endif
+#ifdef USE_GTKHTML
+#endif
+
+#include "../../sylplugin_factory/src/sylplugin_factory.h"
+
+#define COMMIT_READER "commit_reader"
+#define COMMIT_READER_RC "commitreaderrc"
+/*
+#define _(String)   dgettext(COMMIT_READER, String)
+#define N_(String)  gettext_noop(String)
+#define gettext_noop(String) (String)
+*/
+
+#define PLUGIN_NAME N_("CommitReader Plug-in")
+#define PLUGIN_DESC N_("Support to read every commit mail, every day")
+
+#define SYLPF_ID "commit_reader"
+#define SYLPF_OPTION commit_reader_option
+
+#define ENABLE_PRIVATE_BROWSING "enable-private-browsing"
+#define ENABLE_IMAGES "auto-load-images"
+#define ENABLE_SCRIPTS "enable-scripts"
+
+#define ENABLE_SWITCH_TAB "switch-tab"
+#define DEFAULT_FONT_SIZE "default-font-size"
+
+/*#define GET_RC_BOOLEAN(keyarg) g_key_file_get_boolean(g_opt.rcfile, COMMIT_READER, keyarg, NULL)*/
+
+typedef struct _CommitReaderOption CommitReaderOption;
+
+struct _CommitReaderOption {
+  /* General section */
+  /* full path to ghostbiffrc*/
+  gchar *rcpath;
+  /* rcfile */
+  GKeyFile *rcfile;
+  
+#if USE_WEBKITGTK
+  WebKitWebView *html_view;
+#endif
+#if USE_GTKHTML
+  GtkWidget *html_view;
+#endif
+
+#if USE_WEBKITGTK
+  GtkWidget *private_browsing;
+  gboolean private_flag;
+
+  GtkWidget *load_image;
+  gboolean image_flag;
+
+  GtkWidget *scripts;
+  gboolean script_flag;
+
+#endif
+
+  GtkWidget *switch_tab;
+  gboolean switch_tab_flag;
+
+  gboolean show_attach_tab_flag;
+  gint attach_toolbutton_pos;
+
+  gint font_size;
+
+  GtkWidget *folder_entry;
+  gchar *folder_path;
+};
+    
+static void messageview_show_cb(GObject *obj, gpointer msgview,
+                                MsgInfo *msginfo, gboolean all_headers);
+static void exec_commit_reader_menu_cb(void);
+static GtkWidget *create_preference_dialog(CommitReaderOption *option);
+static GtkWidget *create_config_main_page(GtkWidget *notebook, GKeyFile *pkey);
+static GtkWidget *create_config_about_page(GtkWidget *notebook, GKeyFile *pkey);
+static void save_commit_reader_preference(CommitReaderOption *option);
+static GtkWidget *create_htmlview(GtkNotebook *notebook);
+
+static void messageview_change_view_type(MessageView *messageview,
+                                         MessageType type);
+#endif /* __COMMIT_READER_H__ */
